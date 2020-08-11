e697d49ff72a1b29fdf3890e90bb0ebf6bbc19f3
Abdullah Manzoor <abdullahpsoft@gmail.com>
2020-08-11 18:44:47 +0500

    api structure added with passport

---
+++ app/Models/Admin/Products.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
+
+class Products extends Model
+{
+  public  $table = 'h_products';
+
+    //
+}
